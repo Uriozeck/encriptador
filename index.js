2cd76cf8d14b0a66e4e5882c43587ef670e51d2a
Brayan Uriel <brayan.uriel.ramirez@gmail.com>
2024-03-19 23:27:59 -0600

    Creación del encriptador

---
+++ index.js
@@ -0,0 +1,43 @@
+const encriptarTexto = document.querySelector("#texto-encriptado");
+const mensajeEncriptado = document.querySelector("#mensaje-encriptado");
+// const copiarTexto = document.querySelector("#btn-copiar");
+const matrizVocales = [ 
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+    ["é", "enter"],
+    ["í", "imes"],
+    ["á", "ai"],
+    ["ó", "ober"],
+    ["ú", "ufat"],
+];
+
+    function btnEncriptar() {
+        const texto = encriptar(encriptarTexto.value.toLowerCase());
+        mensajeEncriptado.value = texto;
+    }
+
+    function encriptar(fraseAEncriptar) {
+        for (let i = 0; i < matrizVocales.length; i++) {
+            if (fraseAEncriptar.includes(matrizVocales[i][0])) {
+                fraseAEncriptar = fraseAEncriptar.replaceAll(matrizVocales[i][0], matrizVocales[i][1]);
+            }
+        }
+        return fraseAEncriptar;
+    }
+
+    function btnDesencriptar() {
+        const mensaje = desencriptar(encriptarTexto.value.toLowerCase())
+        mensajeEncriptado.value = mensaje;
+    }
+
+    function desencriptar(fraseADesencriptar) {
+        for (i = 0; i < matrizVocales.length; i++) {
+            if (fraseADesencriptar.includes(matrizVocales[i][1])) {
+                fraseADesencriptar = fraseADesencriptar.replaceAll(matrizVocales[i][1], matrizVocales[i][0]);
+            }
+        }
+        return fraseADesencriptar;
+    }
